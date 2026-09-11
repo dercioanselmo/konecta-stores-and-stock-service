f3997b58c66468c420ce355d33f47eda572b3af1
Dercio Anselmo <dercio@branchdev.io>
2026-09-11 06:35:51 +0400

    Test CI

--- src/main/java/com/konecta/stores_stock_service/KonectaStoresAndStockServiceApplication.java
+++ src/main/java/com/konecta/stores_stock_service/KonectaStoresAndStockServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KonectaStoresAndStockServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KonectaStoresAndStockServiceApplication.class, args);
 	}
